24b04b277a660bf1ab14ffd1b6681330d15c0129
Ramiz Ali <ramiz.apk7@gmail.com>
2023-03-07 08:16:10 +0300

    complated assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const isBio = props.bio;
+  let output = "";
+  if (isBio) {
+    output = <p>{props.bio}</p>;
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {output}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
